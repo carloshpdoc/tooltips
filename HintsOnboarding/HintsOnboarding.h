709309b800bd3d34b3d332d5d44647e69c7d30a8
Carlos Henrique <carloshenrique.pdoc@gmail.com>
2021-07-05 21:09:11 -0300

    Initial Commit

---
+++ HintsOnboarding/HintsOnboarding.h
@@ -0,0 +1,18 @@
+//
+//  HintsOnboarding.h
+//  HintsOnboarding
+//
+//  Created by Carlos Henrique on 05/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HintsOnboarding.
+FOUNDATION_EXPORT double HintsOnboardingVersionNumber;
+
+//! Project version string for HintsOnboarding.
+FOUNDATION_EXPORT const unsigned char HintsOnboardingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HintsOnboarding/PublicHeader.h>
+
+
